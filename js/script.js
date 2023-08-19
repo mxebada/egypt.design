dca02bd0ae5096c9fba9c0537769938da5bb8bcf
Mohamed Ebada <111559889+mxebada@users.noreply.github.com>
2023-08-19 13:58:37 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var lina = document.getElementById("copy1")
+lina.onclick = function () {
+    alert("تم نسخ النص.")
+}
+var lina1 = document.getElementById("copy2")
+lina1.onclick = function () {
+    alert("تم نسخ النص.")
+}
+var lina2 = document.getElementById("copy3")
+lina2.onclick = function () {
+    alert("تم نسخ النص.")
+}
